4d96e8c75740c7d49280d5e6218f3a3917e9c90c
Hendrik Seffler <hse@spreadshirt.net>
2016-11-23 16:53:13 +0100

    Adding a little debugging for the version

--- manifests/init.pp
+++ manifests/init.pp
@@ -72,7 +72,7 @@ class gitlab_ci_multi_runner (
                 /^(5.*|6.*)/ => '0.4.2-1',
                 default      => 'latest',
             },
-            'debian' => '1.7.1',
+            'debian' => 'latest',
             default  => 'There is no spoon',
         }
     } else {
